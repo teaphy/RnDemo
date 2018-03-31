377be4ef4f63ba226af6be96a718d5ca2fa9cd06
teaphy <334679056@qq.com>
2018-03-31 15:24:34 +0800

    1.LoginView 2.20180331

--- App.js
+++ App.js
@@ -4,90 +4,14 @@
  * @flow
  */
 
-import React, { Component } from 'react';
-import {
-  Platform,
-  StyleSheet,
-  Text,
-  View,
-  Image
-} from 'react-native';
-
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' +
-    'Cmd+D or shake for dev menu',
-  android: 'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
-
-let Dimensions = require('Dimensions')
-let width = Dimensions.get('window').width
-
-let cols = 3
-let wItem = 100
-let leftM = (width - wItem * cols) / (cols + 1) 
-
-// 解析json
-let bags = require('./bags.json')
+import React, {Component} from 'react';
+import LoginView from './loginView'
+// let LoginView = require('./loginView');
 
 export default class App extends Component {
-
-
-  renderAllBagView(){
-    let bagViews = []
-    for(let i = 0; i < bags.data.length; i++) {
-      let {iconUrl, title} = bags.data[i]
-
-      let imgReq =
-      console.log(`iconUrl: ${iconUrl}`)
-      console.log(`imgReq: ${imgReq}`)
-      // 将view添加到数组
-      bagViews.push(
-        <View  key = {i} style={styles.bagContainer}>
-          <Image  source={  require( './imgs/shoutibao.png') }  style={styles.bagImg}/>
-          <Text style={styles.bagTitle}>
-            {title}
-          </Text> 
-        </View>
-      )
-    }
-
-    // 返回Views
-    return bagViews
-  }
-
-  render() {
-    return (
-      <View style={styles.container}>
-        {this.renderAllBagView()}
-      </View>
-    );
-  }
+	render() {
+		return (
+			<LoginView/>
+		);
+	}
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flexDirection: 'row',
-    flexWrap: 'wrap',
-   
-  },
-  bagContainer: {
-    width: wItem,
-    height: 120,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F1F1F1',
-    marginLeft: leftM,
-    marginTop: leftM,
-  },
-  bagImg: {
-    width: 80,
-    height: 80
-  
-  },
-
-  bagTitle: {
-    color: 'red',
-  }
-
-});
---
+++ loginView.js
@@ -0,0 +1,125 @@
+import React, {Component} from 'react'
+import {
+	View,
+	StyleSheet,
+	Text,
+	Image,
+	TextInput,
+	Button,
+} from 'react-native'
+
+let Dimensions = require('Dimensions');
+let wScreen = Dimensions.get('window').width;
+
+export default class LoginView extends Component{
+
+	render() {
+		
+		return (
+			<View style={styles.container}>
+				<Image
+					source={require('./imgs/icon_head.png')}
+					style={styles.imageHeadStyle} />
+				
+				<View style={styles.accountContainer}>
+					<TextInput style={styles.accountInputStyle} placeholder={'请输入账号'}/>
+					<TextInput style={styles.accountInputStyle} placeholder={'请输入密码'} secureTextEntry={true}/>
+				</View>
+				
+				<View style={styles.loginBtnContainer}>
+					<Text style={{color: 'white', fontSize: 16}}>
+						登录
+					</Text>
+				</View>
+				
+				<View style={styles.settingContainer}>
+					<Text>
+						无法登录
+					</Text>
+					
+					<Text>
+						注册
+					</Text>
+				</View>
+				
+				<View style={styles.loginOtherContainer}>
+					<Text>
+						其他登录
+					</Text>
+					
+					<Image source={ require('./imgs/icon_qq.png') } style={styles.imageLoginStyle}/>
+					<Image source={ require('./imgs/icon_wx.png') } style={styles.imageLoginStyle}/>
+					<Image source={ require('./imgs/icon_wx.png') } style={styles.imageLoginStyle}/>
+				</View>
+			</View>
+		);
+	}
+	
+
+}
+
+const styles = StyleSheet.create({
+	container: {
+		flex:1,
+		alignItems:'center',
+		backgroundColor: "#dddddd"
+	},
+	
+	imageHeadStyle: {
+		width: 60,
+		height: 60,
+		marginTop: 40,
+		marginBottom: 20,
+		borderRadius:30,
+		borderColor:'white',
+		borderWidth: 1,
+	},
+	
+	accountContainer: {
+		
+		marginLeft: 20,
+		marginRight: 20,
+		width: wScreen - 40,
+	},
+	
+	accountInputStyle: {
+		marginBottom: 1,
+		backgroundColor: 'white',
+	},
+	
+	loginBtnContainer: {
+		backgroundColor: 'blue',
+		marginLeft: 20,
+		marginRight: 20,
+		width: wScreen - 40,
+		marginTop: 10,
+		borderRadius: 6,
+		height: 36,
+		justifyContent:'center',
+		alignItems:'center'
+	},
+	
+	settingContainer: {
+		marginLeft: 20,
+		marginRight: 20,
+		width: wScreen - 40,
+		marginTop: 15,
+		flexDirection: 'row',
+		justifyContent: 'space-between'
+	},
+	
+	loginOtherContainer: {
+		width: wScreen - 40,
+		position: 'absolute',
+		bottom: 10,
+		flexDirection: 'row',
+		alignItems: 'center'
+	},
+	
+	imageLoginStyle: {
+		width: 36,
+		height: 36,
+		marginLeft: 8,
+		borderRadius:6,
+	}
+});
