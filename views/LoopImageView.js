1987e92296079e97f01c1ce80de50e06f3c5e540
teaphy <334679056@qq.com>
2018-04-02 19:40:35 +0800

    1.LoopImageView 2.20180401

--- views/LoopImageView.js
+++ views/LoopImageView.js
@@ -6,29 +6,49 @@ import {
 	Image,
 	TextInput,
 	Button,
-} from 'react-native'
+	ScrollView,
+} from 'react-native';
 
 let Dimensions = require('Dimensions');
 let wScreen = Dimensions.get('window').width;
 
-let imgDatas = require('./jsons/LoopImageData');
+let imgsData = require('../data/LoopImageData');
 
 export default class LoopImageView extends Component{
 
 	render() {
-		
+		//
 		return (
 			<View style={styles.container}>
-				<Text>
-					LoopImageView
-				</Text>
+				<ScrollView
+					ref = "scrollView"
+					showsHorizontalScrollIndicator={false}
+					pagingEnabled={true}
+					horizontal={true}>
+					{LoopImageView.renderLoopImageView()}
+				</ScrollView>
+				
 			</View>
 		);
 	}
-	
-
+	static renderLoopImageView(){
+		let views = [];
+		
+		for (let i = 0; i < imgsData.data.length; i++) {
+			let {img, title} = imgsData.data[i];
+			views.push(
+				<View key={i}>
+					<Image source={{uri: img}} style={styles.loopImgStyle}/>
+				</View>
+			);
+		}
+		return views;
+	}
 }
 
 const styles = StyleSheet.create({
-
+	loopImgStyle: {
+		width:wScreen,
+		height:120,
+	}
 });
